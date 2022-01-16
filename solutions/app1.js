32cf63b68586d306ca3a04844d53c13d20a72bbf
eaniko <69327188+eaniko@users.noreply.github.com>
2022-01-17 00:09:44 +0100

    firstcommit

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,12 @@
+const converterPromise = (arr) =>
+	new Promise((resolve, reject) => {
+		try {
+			const converterPromiseStringsArray = arr.map((str) =>
+				str.toLocaleconverterPromise()
+			);
+			resolve(converterPromiseStringsArray);
+		} catch (error) {}
+		reject(new Error('Error: Error: Not all elements are string type!'));
+	});
 
-
-
-
-
-
-// export default converterPromise;
+export default converterPromise;
--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,16 @@
+'use strict';
 
+const sorterPromise = (array) => {
+	return new Promise((resolve, reject) => {
+		const filteredArray = array.filter((item) => {
+			return typeof item === 'string';
+		});
+		if (filteredArray.length != array.length) {
+			reject(`Error: Not all elements are string type!`);
+		} else {
+			resolve(filteredArray.sort());
+		}
+	});
+};
 
-
-
-
-
-// export default sorterPromise;
+export default sorterPromise;
